b6ded630e0a9a2aff1a1609346687c3b9ee5c7b6
Maxim Sachko <114748761+SachkoMaxim@users.noreply.github.com>
2025-10-25 15:29:03 +0300

    feat: added player sprite - added player sprites; - added idle and walking animations

--- Assets/Scripts/Characters/Player/PlayerController.cs
+++ Assets/Scripts/Characters/Player/PlayerController.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
-
+    private Animator animator;
     private Vector2 input;
 
-    public float moveSpead = 100;
+    public float moveSpeed = 30f;
+
+    private static readonly int isMoving = Animator.StringToHash("isMoving");
+    private static readonly int moveX = Animator.StringToHash("moveX");
+    private static readonly int moveY = Animator.StringToHash("moveY");
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
-
-    void FixedUpdate()
-    {
-        if (input.x != 0) input.y = 0;
-        rb.velocity = new Vector2(input.x * moveSpead * Time.deltaTime, input.y * moveSpead * Time.deltaTime);
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,5 +26,28 @@ public class Player : MonoBehaviour
     {
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if (input.x != 0) input.y = 0;
+
+        Vector2 movement = input * moveSpeed * Time.fixedDeltaTime;
+        rb.velocity = movement;
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        if (input != Vector2.zero)
+        {
+            animator.SetFloat(moveX, input.x);
+            animator.SetFloat(moveY, input.y);
+            animator.SetBool(isMoving, true);
+        }
+        else
+        {
+            animator.SetBool(isMoving, false);
+        }
     }
 }
